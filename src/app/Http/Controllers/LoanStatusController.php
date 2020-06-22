4f593015848e4ab3eb2fa912d34c7419a47a3e39
Anthony Barasa <anthony.barasa@switchlinkafrica.co.ke>
2020-06-22 11:58:23 +0300

    dd

--- src/app/Http/Controllers/LoanStatusController.php
+++ src/app/Http/Controllers/LoanStatusController.php
@@ -19,8 +19,9 @@ class LoanStatusController extends Controller
         $message = new Status;
         $message->customeridnumber = $request->input('customeridnumber');
         $message->customermobilenumber = $request->input('customermobilenumber');
+        $ngao->msisdn = $request->input('msisdn');
         
-        dd($message);
+        dd($ngao);
         // $ngao = Status::paginate(15);
         $withdraw = DB::table('statuses')->value('loanbalance');
         // dd($withdraw);
